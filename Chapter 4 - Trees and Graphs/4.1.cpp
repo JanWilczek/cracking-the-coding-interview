8a2f300d49dddae62910b3982dc72278a3711c81
Jan Wilczek <janekw@vp.pl>
2020-10-14 14:15:43 +0200

    Exercise 4.1.

---
+++ Chapter 4 - Trees and Graphs/4.1.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+
+struct Node{
+    Node *left{nullptr}, *right{nullptr};
+};
+
+
+int treeHeight(Node* root, bool& isBalanced)
+{
+	if (!root) return 0;
+
+	auto leftSubtreeHeight = treeHeight(root->left, isBalanced);
+	auto rightSubtreeHeight = treeHeight(root->right, isBalanced);
+
+	if (std::abs(leftSubtreeHeight - rightSubtreeHeight) > 1) isBalanced= false;
+
+	return std::max(leftSubtreeHeight, rightSubtreeHeight) + 1;
+}
+
+bool isBalanced(Node* root)
+{
+	bool result = true;
+	treeHeight(root, result);
+	return result;
+}
+
+void destroyTree(Node* root)
+{
+    if (root->right)
+    {
+        destroyTree(root->right);
+        delete root->right;
+    }
+
+    if (root->left)
+    {
+        destroyTree(root->left);
+        delete root->left;
+    }
+}
+
+void isBalancedTest()
+{
+    Node* unbalanced = new Node{new Node{new Node{new Node, nullptr}, nullptr}, new Node};
+    Node* balanced = new Node{new Node{new Node, nullptr}, new Node};
+
+    assert(!isBalanced(unbalanced));
+    assert(isBalanced(balanced));
+
+    destroyTree(unbalanced);
+    delete unbalanced;
+
+    destroyTree(balanced);
+    delete balanced;
+}
+
+int main()
+{
+    isBalancedTest();
+}
