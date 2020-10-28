2303282a5707f218a24ab8aedf199976a3b40e88
Jan Wilczek <janekw@vp.pl>
2020-10-28 19:00:06 +0100

    Exercise 5.6 book solution.

--- Chapter 5 - Bit Manipulation/5.6.cpp
+++ Chapter 5 - Bit Manipulation/5.6.cpp
@@ -16,11 +16,24 @@ constexpr unsigned int swapOddAndEven(unsigned int a)
     return a;
 }
 
+// assuming we have 32 bits…
+constexpr unsigned int swapOddAndEven2(unsigned int a)
+{
+	auto maskOdd = 0b10101010101010101010101010101010u;
+	auto maskEven = maskOdd >> 1;
+
+	return ((a & maskOdd) >> 1) | ((a & maskEven) << 1);
+}
+
 void swapOddAndEvenTest()
 {
     static_assert(swapOddAndEven(0b11010u) == 0b100101u);
     static_assert(swapOddAndEven(0b010011u) == 0b100011u);
     static_assert(swapOddAndEven(0b110100u) == 0b111000u);
+
+    static_assert(swapOddAndEven2(0b11010u) == 0b100101u);
+    static_assert(swapOddAndEven2(0b010011u) == 0b100011u);
+    static_assert(swapOddAndEven2(0b110100u) == 0b111000u);
 }
 
 int main()
